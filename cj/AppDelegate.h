6cc41007f2d20dc451a9ce31a47e36e692dfb950
cj <cj@WhosvMBP.local>
2015-05-09 00:30:21 +0800

    first

---
+++ cj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cj
+//
+//  Created by cj on 15/5/9.
+//  Copyright (c) 2015年 cj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cj
+//
+//  Created by cj on 15/5/9.
+//  Copyright (c) 2015年 cj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
